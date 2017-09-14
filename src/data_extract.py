dca47259f73bdd8d82d41c55f2cf48883b1813a2
qsong <songqiguang@gmail.com>
2017-09-14 12:45:51 +0200

    add dict

--- src/data_extract.py
+++ src/data_extract.py
@@ -16,7 +16,8 @@ class data_extract(object):
     This module provides data_extract
     '''
     recommendation_url = "http://www.pk2018.net/game_show.asp"
-
+    tip_dict = {}
+    
     def __init__(self):
         '''
         Constructor
@@ -35,22 +36,22 @@ class data_extract(object):
             tip_table_td = elm.findAll ('td')
             if len(tip_table_td) < 10:
                 continue
-            tip_season = UnicodeDammit (tip_table_td[0].text,  ["gb2312"]).unicode_markup
-            game_type = UnicodeDammit (tip_table_td[1].text,  ["gb2312"]).unicode_markup
-            game_time = UnicodeDammit (tip_table_td[2].text,  ["gb2312"]).unicode_markup
-            game_home = UnicodeDammit (tip_table_td[3].text,  ["gb2312"]).unicode_markup
-            game_odd = UnicodeDammit (tip_table_td[4].text,  ["gb2312"]).unicode_markup
-            game_guest = UnicodeDammit (tip_table_td[5].text,  ["gb2312"]).unicode_markup
-            tip_info = UnicodeDammit (tip_table_td[6].text,  ["gb2312"]).unicode_markup.split('/')[0]
-            tip_value = UnicodeDammit (tip_table_td[6].text,  ["gb2312"]).unicode_markup.split('/')[1]
-            game_score = UnicodeDammit (tip_table_td[7].text,  ["gb2312"]).unicode_markup
-            tip_result = UnicodeDammit (tip_table_td[8].text,  ["gb2312"]).unicode_markup
-            tip_time = UnicodeDammit (tip_table_td[9].text,  ["gb2312"]).unicode_markup
-            tip_user_name = UnicodeDammit (tip_table_td[10].text,  ["gb2312"]).unicode_markup
+            self.tip_dict['tip_season'] = UnicodeDammit (tip_table_td[0].text,  ["gb2312"]).unicode_markup   #第30期
+            self.tip_dict['game_type'] = UnicodeDammit (tip_table_td[1].text,  ["gb2312"]).unicode_markup    #欧冠杯    
+            self.tip_dict['game_time'] = UnicodeDammit (tip_table_td[2].text,  ["gb2312"]).unicode_markup    #09-14 02:45    
+            self.tip_dict['game_home'] = UnicodeDammit (tip_table_td[3].text,  ["gb2312"]).unicode_markup    #皇家马德里
+            self.tip_dict['game_odd'] = UnicodeDammit (tip_table_td[4].text,  ["gb2312"]).unicode_markup     #三球半
+            self.tip_dict['game_guest'] = UnicodeDammit (tip_table_td[5].text,  ["gb2312"]).unicode_markup   #希腊人竞技
+            self.tip_dict['tip_info'] = UnicodeDammit (tip_table_td[6].text,  ["gb2312"]).unicode_markup.split('/')[0]   #皇家马德里
+            self.tip_dict['tip_value'] = UnicodeDammit (tip_table_td[6].text,  ["gb2312"]).unicode_markup.split('/')[1]  #0.85
+            self.tip_dict['game_score'] = UnicodeDammit (tip_table_td[7].text,  ["gb2312"]).unicode_markup   #-
+            self.tip_dict['tip_result'] = UnicodeDammit (tip_table_td[8].text,  ["gb2312"]).unicode_markup   #等待
+            self.tip_dict['tip_time'] = UnicodeDammit (tip_table_td[9].text,  ["gb2312"]).unicode_markup     #09-13 20:24    
+            self.tip_dict['tip_user_name'] = UnicodeDammit (tip_table_td[10].text,  ["gb2312"]).unicode_markup   #我要下天台
             tip_user_if_soup = tip_table_td[10].findAll('a' , href=True)
-            tip_user_id = tip_user_if_soup[0].attrs['href'].split("=")[1]
+            self.tip_dict['tip_user_id'] = tip_user_if_soup[0].attrs['href'].split("=")[1]                   #16135
             
-            print(tip_user_name)
+            print(self.tip_dict)
 
         return
 
